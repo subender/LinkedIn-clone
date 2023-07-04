cb49ab2b86ceafb76dbee0e1031f1b213e712916
subender <subender.io@outlook.com>
2023-07-04 16:46:38 +0530

    added Register api code.

--- src/API/AuthAPI.jsx
+++ src/API/AuthAPI.jsx
@@ -1,1 +1,11 @@
-export const LoginAPI = () => {};
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+
+export const RegisterAPI = async (email, password) => {
+  try {
+    let response = await createUserWithEmailAndPassword(auth, email, password);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
