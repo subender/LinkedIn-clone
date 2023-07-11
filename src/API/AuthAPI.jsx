7f934d9a41f16da3bb84a944e2107bbf77400ab6
subender <subender.io@outlook.com>
2023-07-11 18:09:39 +0530

    Added login api code.

--- src/API/AuthAPI.jsx
+++ src/API/AuthAPI.jsx
@@ -1,11 +1,23 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebaseConfig";
+
+export const LoginAPI = (email, password) => {
+  try {
+    let response = signInWithEmailAndPassword(auth, email, password);
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
 
 export const RegisterAPI = async (email, password) => {
   try {
     let response = await createUserWithEmailAndPassword(auth, email, password);
     return response;
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    return err;
   }
 };
